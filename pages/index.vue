3050223caf9674262f2785867041a1873214c856
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2023-11-30 20:17:23 +0300

    hello

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>Hello</h1>
+      <h2>This is a test page</h2>
+      <img src="/img/1.png" alt="Discover Nuxt 3" />
+      
+    </div>
+    <div class="wrapper">
+        
+    </div>
+  </template>
