43a2fd3fa5ad504cec6debea462ebce1f68b103f
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2023-11-30 20:32:58 +0300

    modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.scss']
+  css: ['~/assets/css/main.scss'],
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/web-vitals'
+  ]
 })
