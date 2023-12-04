938cb595069306e7019acc8206a3dde8e0da1d48
Vladimir Litvintsev <vl.litvintsev@yandex.ru>
2023-12-05 01:10:59 +0300

    fix: pixelated effect constants change

--- utils/pixelatedAppearanceEffect/constants.ts
+++ utils/pixelatedAppearanceEffect/constants.ts
@@ -1,6 +1,6 @@
-export const START_PIXEL_SIZE = 30;
+export const START_PIXEL_SIZE = 13;
 export const ANIMATION_PROPERTIES = {
-  DURATION: 4,
+  DURATION: 0.35,
   DELAY: 0.1,
   STEPS_COUNT: 8,
 };
