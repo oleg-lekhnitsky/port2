0d79812af2a523106ec9e1982444557f0918c6d8
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2024-05-03 16:31:51 +0300

    gallery-styling

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -27,13 +27,13 @@
       <NuxtImg src="/img/28.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" />
 
       <NuxtImg src="/img/14.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" />
-      <h1 style="text-align: center;">(Apple) <br> specifies a grid item's size and location within a grid row by
-        contributing a line, a span, or nothing automatic to its grid placement, </h1>
       <NuxtImg src="/img/9.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" />
-
+      
       <NuxtImg src="/img/25.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" />
+        <h1 style="text-align: center;">(Apple) <br> specifies a grid item's size and location within a grid row by contributing a line, a span, or nothing automatic to its grid placement, </h1>
 
       <NuxtImg src="/img/21.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" />
+      <NuxtImg src="/img/11.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" />
 
       <!-- <NuxtImg src="/img/8.png" format="webp" alt="Discover Nuxt 3" :placeholder="[5]" /> -->
     </div>
@@ -45,11 +45,14 @@
 
 
 <style lang="scss" scoped>
-.NuxtImg {
+img {
   height: auto;
   width: 100%;
   min-width: 100%;
-  min-height: 200px;
+  min-height: 300px;
+  overflow: hidden;
+  aspect-ratio: 1080/1350;
+  object-fit: cover;
 }
 
 caption {
@@ -61,10 +64,11 @@ caption {
 <style lang="scss" scoped>
 .wrapper {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 8px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0px;
   flex-direction: column;
-  padding: 120px 12px;
+  padding: 70px 0px;
+  padding-bottom: 0;
   max-width: 100vw;
   box-sizing: border-box;
 }
