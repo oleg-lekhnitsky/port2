fab71726cc255074b0dcda768d4bdfb3f333b48f
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2024-05-04 01:18:53 +0300

    content

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,29 @@
 <script setup lang="ts">
 const images = ref<ISliderImage[]>([
 	{
-		src: '/img/20.png',
+		src: '/img/11.png',
 		alt: 'Image 1',
 		color: '#F97600',
 		aspectRatio: .8,
 	},
+
 	{
-		src: '/img/12.png',
-		alt: 'Image 2',
-		color: '#0b161c',
-		aspectRatio: .669,
-	},
-	{
-		src: '/img/10.png',
+		src: '/img/14File .png',
 		alt: 'Image 3',
 		color: '#9cc2de',
-		aspectRatio: 1,
+		aspectRatio: .8,
 	},
 	{
-		src: '/img/9.png',
+		src: '/img/16File .png',
+		alt: 'Image 2',
+		color: '#0b161c',
+		aspectRatio: .8,
+	},
+	{
+		src: '/img/20.png',
 		alt: 'Image 4',
 		color: '#6a1d09',
-		aspectRatio: 1,
+		aspectRatio: .8,
 	},
 	{
 		src: '/img/8.png',
